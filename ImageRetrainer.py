3831112696b37a1adf87fe8aaddb95860fbdacde
Ali Siddiqui <alinsiddiqui@gmail.com>
2017-11-14 04:36:51 -0800

    Download the inception model and load it into a graph

--- ImageRetrainer.py
+++ ImageRetrainer.py
@@ -1,28 +1,51 @@
+import os
 import urllib
+import tarfile
+
+import tensorflow as tf
 
 num_training_steps = 5000
-inception_model_url = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
-inception_model_filename = 'inceptionV3.tgz'
+inception_url = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+inception_dir_filepath = '/tmp/inception/'
+inception_filepath = inception_dir_filepath + 'classify_image_graph_def.pb'
+
+class NeuralNetwork(object):
+    def __init__(self):
+        self.graph = tf.Graph()
+
+        with self.graph.as_default():
+            with tf.gfile.FastGFile(inception_filepath, 'rb') as f:
+                graph_def = tf.GraphDef()
+                graph_def.ParseFromString(f.read())
+                tf.import_graph_def(graph_def, name='')
+
+        self.session = tf.Session(graph=self.graph)
+
+
+    '''
+    Initialize a softmax layer to the inception model
+    '''
+    def add_final_layer(self):
+        pass
+
+    '''
+    Evaluation step is used to evaluate the accuracy of the model
+    '''
+    def add_evaluation_step(self):
+        pass
 
 '''
 Download and initialize the inception model
 '''
 def initialize_model():
-    urllib.urlretrieve(inception_model_url, inception_model_filename)
+    zipped_filepath = inception_dir_filepath + inception_url.split('/')[-1]
 
-    return None
+    urllib.urlretrieve(inception_url, zipped_filepath)
+    if not os.path.exists(inception_dir_filepath):
+        os.makedirs(inception_dir_filepath)
+    tarfile.open(zipped_filepath, 'r:gz').extractall(inception_dir_filepath)
 
-'''
-Initialize a softmax layer to the inception model
-'''
-def add_final_layer(model):
-    pass
-
-'''
-Evaluation step is used to evaluate the accuracy of the model
-'''
-def add_evaluation_step(model):
-    pass
+    return NeuralNetwork()
 
 '''
 Get arrays of training and testing images
@@ -32,8 +55,6 @@ def get_images():
 
 if __name__ == '__main__':
     model = initialize_model()
-    add_final_layer(model)
-    add_evaluation_step(model)
 
     images = get_images()
 
